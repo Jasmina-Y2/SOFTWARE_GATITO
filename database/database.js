5649a2a820ca433709de3ccdaeb87a69219a3dc8
JHON QUISPE LLANCARI <yhonmaycolquispellancare1010@gmail.com>
2025-05-07 15:30:23 -0500

    panel de directorr

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 var mysql  = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'b2aaeo3tkn0gkkcqrpy5-mysql.services.clever-cloud.com',
-  user     : 'ufztbhpy9wsyfumn',
-  password : 'npTXFSdSKABD3JskNq7o',
-  database : 'b2aaeo3tkn0gkkcqrpy5',
+  host     : 'https://github.com/Jasmina-Y2/SOFTWARE_GATITO',
+  user     : 'yhoncito24',
+  password : 'teamoyhon1',
+  database : 'SOFTWARE_GATITO',
   port     : 3306,
 });
 
@@ -12,3 +12,5 @@ connection.connect();
 
 
 module.exports = connection;
+
+
